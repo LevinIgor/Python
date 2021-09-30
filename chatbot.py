68007b64d4dc505941cb0e4d320a72bd50772dfe
Ihor <lytghzys@gmail.com>
2021-09-30 18:34:04 +0300

    ChatBot 3 stage

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,13 @@
 
-print("Hello! My name is KahovChatBot.")
-print("I was created in 2021.")
-print("Please, remind me your name.")
+print("""Hello! My name is KahovChatBot.
+I was created in 2021.
+Please, remind me your name.""")
 print("What a great name you have, " + input() + "!")
+print("""Let me guess your age.
+Enter remainders of dividing your age by 3, 5 and 7.""")
+
+remainder3 = input()
+remainder5 = input()
+remainder7 = input()
+
+print("Your age is " + str((int(remainder3) * 70 + int(remainder5) * 21 + int(remainder7) * 15) % 105) +"; that's a good time to start programming!") 
