3da828decd409dea1d56a754f96da9bf6671e43d
Ihor <lytghzys@gmail.com>
2021-09-30 18:59:54 +0300

    ChatBot 5 stage

--- chatbot.py
+++ chatbot.py
@@ -11,11 +11,17 @@ print("What a great name you have, " + input() + "!")
 
 
 # Stage 3
-print("""Let me guess your age.
+while True:
+    print("""Let me guess your age.
 Enter remainders of dividing your age by 3, 5 and 7.""")
-remainder3 = input()
-remainder5 = input()
-remainder7 = input()
+    remainder3 = input()
+    remainder5 = input()
+    remainder7 = input()
+    if len(remainder7)>0 and len(remainder3)>0 and len(remainder5)>0:
+        break
+    else:
+          print("Error: non correct number!")
+        
 print("Your age is " + str((int(remainder3) * 70 + int(remainder5) * 21 + int(remainder7) * 15) % 105) +"; that's a good time to start programming!") 
 
 
@@ -25,4 +31,20 @@ for i in range(int(input())+1):
     print(str(i)+" !")
 print("Completed, have a nice day!")
 
+
+# Stage 5
+print("""Let's test your programming knowledge.
+Загадка Жака Фреско. Сколько?
+1. 10.
+2. Воробей.
+3. Киев.
+4. Хто я.""")
+while True:
+    if input() == "4":
+        break
+    else:
+        print("Please, try again.")
+
+print("""Completed, have a nice day!
+Congratulations, have a nice day!""")
 
