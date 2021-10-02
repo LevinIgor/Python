04bcac9af933c7761790d93528066d2514898f7a
Ihor <lytghzys@gmail.com>
2021-10-02 18:44:36 +0300

    HangMan: Stage 5

--- HangMan/HangMan.py
+++ HangMan/HangMan.py
@@ -1,13 +1,26 @@
 import random
 
-_words = ["JavaScript", "Java","Python", "Ruby"]
+_words = ["javascript", "java","python", "ruby"]
 _rngWord = _words[random.randrange(4)]
+_word = ""
 
-print("Guess the word: " + _rngWord[0:len(_rngWord)-3]+"---")
+for i in range(len(_rngWord)):
+    _word = _word + "-"
+print(_word)
 
-if input().lower() == _rngWord.lower():
-    print("You survived!")
-else:
-    print("You lost!")
+for i in range(8):
+    print("Input a letter:")
+    _input = input()
+    if  _input in _rngWord:
+        for i in range(len(_rngWord)):
+            if _input == _rngWord[i]:
+                _word = _word[:i] + _input + _word[i+1:]
+    print(_word)
+
+print("""Thanks for playing!
+We'll see how well you did in the next stage""")
+
+
+
 
 
