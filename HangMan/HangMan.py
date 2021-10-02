10cec9abaf217d74a844bcd485fe287b26bec430
Ihor <lytghzys@gmail.com>
2021-10-02 17:41:16 +0300

    HangMan: Stage 2

--- HangMan/HangMan.py
+++ HangMan/HangMan.py
@@ -5,4 +5,10 @@ print("""HANGMAN
 The game will be available soon.""")
 
 
+# Stage 2
+print("Guess the word:")
+if input() == "example":
+    print("You survived!")
+else:
+    print("You lost!")
 
