d890b6c00ce16e2e20193894232eefde14a1eef6
Ihor <lytghzys@gmail.com>
2021-10-02 19:38:16 +0300

    HangMan: Stage 7

--- HangMan/HangMan.py
+++ HangMan/HangMan.py
@@ -1,42 +1,62 @@
 import random
 
+def replaceDash(_inputChar, _randomWord, _dashWord):
+    for i in range(len(_randomWord)):
+        if _inputChar == _randomWord[i]:
+            _dashWord =  _dashWord[:i] + _inputChar + _dashWord[i+1:]
+    return _dashWord
+
+def isNormalInput(_inputChar):
+    if len(_inputChar) > 1:
+        print("You should input a single letter.")
+        return False
+    
+    if _inputChar.isupper():
+        print("Please enter a lowercase English letter.")
+        return False
+    return True
+
+def isWin(_dashWord):
+    if "-" not in _dashWord:
+        print("""You guessed the word!
+You survived!""")
+        return True
+    else:
+        return False
+
+
+
 _words = ["javascript", "java","python", "ruby"]
-_rngWord = _words[random.randrange(len(_words))]
-_word = ""
-
-for i in range(len(_rngWord)):
-    _word = _word + "-"
-print(_word)
-
+_randomWord = _words[random.randrange(len(_words))]
+_dashWord = ""
 _points = 8
 
-while _points > 0:
-    _input = input("Input a letter:")
-    if  _input in _rngWord and _input not in _word:
-        for i in range(len(_rngWord)):
-            if _input == _rngWord[i]:
-                _word = _word[:i] + _input + _word[i+1:]
-    else:
-        _points = _points - 1
 
-        if _input in _word:
+for i in range(len(_randomWord)):
+    _dashWord = _dashWord + "-"
+print(_dashWord)  
+
+while _points > 0:
+    _inputChar = input("Input a letter:")
+
+    if isNormalInput(_inputChar):
+        if  _inputChar in _randomWord and _inputChar not in _dashWord:
+           _dashWord = replaceDash(_inputChar,_randomWord,_dashWord)
+    
+        if _inputChar not in _randomWord:
+            _points = _points - 1
+
+        if _inputChar in _dashWord:
             print("No improvements")
         else:
             print("That letter doesn't appear in the word")
+
     print(" ")
-    print(_word)
-
-    if "-" not in _word:
-        print("""You guessed the word!
-You survived!""")
+    print(_dashWord)
+    
+    if isWin(_dashWord):
         break
-
 
 print(" ")
 print("""Thanks for playing!
 We'll see how well you did in the next stage""")
-
-
-
-
-
