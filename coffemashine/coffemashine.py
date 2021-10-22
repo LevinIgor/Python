6023697e75beac6a5b4bdfc7586852b2b146d0f5
Ihor <lytghzys@gmail.com>
2021-10-22 23:47:02 +0300

    stage 5

--- coffemashine/coffemashine.py
+++ coffemashine/coffemashine.py
@@ -1,29 +1,72 @@
-import math
 
-print("Write how many ml of water the coffee machine has:")
-water = int(input())
-
-print("Write how many ml of milk the coffee machine has:")
-milk = int(input())
-
-print("Write how many grams of coffee beans the coffee machine has:")
-coffee =int(input()) 
-
-print("Write how many cups of coffee you will need:")
-count = int(input())
-
-countMilk = milk/50
-countWater = water/200
-countCoffee = coffee/15
-print("m:"+str(countMilk)+" w:"+str(countWater)+" c:"+str(countCoffee))
-minimum =min(countMilk,countCoffee,countWater)
+def printIng():
+    print("The coffee mashine has:")
+    print(str(water) + " of water")
+    print(str(milk) + " of milk")
+    print(str(coffee) + " of coffee")
+    print(str(cup) + " of disposable cups")
+    print(str(money) + " of money")
+    print()
 
 
-if(minimum==count):
-    print("Yes, I can make that amount of coffee")
-if minimum > count:
-    print("Yes, I can make that amount of coffee (and even "+str(math.floor(minimum))+" more than that)")
-if minimum < count:
-    print("No, I can make only "+str(math.floor(minimum))+" cups of coffee")
+money = 550
+water = 400
+milk = 540
+coffee = 120
+cup = 9
+
+while True:
+
+    print("Write action (buy, fill, take, remaining, exit):")
+    action = input()
+
+    if action == "b":
+        print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back:")
+        buy =input()
+        if buy == "1":
+            if water - 250 > 0 and coffee - 16 > 0 and cup > 0:
+                water = water - 250
+                coffee = coffee - 16
+                cup = cup -1
+                money = money + 4
+            else:
+                print("Недостаточно ингредиентов")
+        if buy == "2":
+            if water -350 > 0 and milk - 75 > 0 and coffee - 20 > 0 and cup > 0:
+                water = water - 350
+                milk = milk - 75
+                coffee = coffee - 20
+                cup = cup -1
+                money = money + 7
+            else:
+                print("Недостаточно ингредиентов")
+        if buy == "3":
+            if water - 200 > 0 and milk - 100 > 0 and coffee - 12 > 0 and cup > 0:
+                water = water - 200
+                milk = milk - 100
+                coffee = coffee - 12
+                cup = cup -1
+                money = money + 6
+            else:
+                print("Недостаточно ингредиентов")
+        if buy == "b":
+            continue
+    if action == "t":
+        print("I gave you " + str(money))
+        money = 0
+    if action == "f":
+        print("Write how many ml of water you want to add:")
+        water = water + int(input())
+        print("Write how many ml of milk you want to add:")
+        milk = milk + int(input())
+        print("Write how many grams of coffee beans you want to add:")
+        coffee = coffee + int(input())
+        print("Write how many disposable coffee cups you want to add:")
+        cup = cup + int(input())
+    if action == "r":
+        printIng()
+    if action == "e":
+        break
+
 
 
