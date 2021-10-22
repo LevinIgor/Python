55a59d6f2b8b45116abb9e0c650dadff5b643c88
Ihor <lytghzys@gmail.com>
2021-10-22 23:04:06 +0300

    stage 3

--- coffemashine/coffemashine.py
+++ coffemashine/coffemashine.py
@@ -1,9 +1,29 @@
+import math
+
+print("Write how many ml of water the coffee machine has:")
+water = int(input())
+
+print("Write how many ml of milk the coffee machine has:")
+milk = int(input())
+
+print("Write how many grams of coffee beans the coffee machine has:")
+coffee =int(input()) 
+
 print("Write how many cups of coffee you will need:")
-count = input()
+count = int(input())
 
-print("For "+count+ " cups of coffee you will need")
+countMilk = milk/50
+countWater = water/200
+countCoffee = coffee/15
+print("m:"+str(countMilk)+" w:"+str(countWater)+" c:"+str(countCoffee))
+minimum =min(countMilk,countCoffee,countWater)
 
-print(str(int(count)*200) + " ml of water")
-print(str(int(count)*50) + " ml of milk")
-print(str(int(count)*15) + " g of coffee beans")
+
+if(minimum==count):
+    print("Yes, I can make that amount of coffee")
+if minimum > count:
+    print("Yes, I can make that amount of coffee (and even "+str(math.floor(minimum))+" more than that)")
+if minimum < count:
+    print("No, I can make only "+str(math.floor(minimum))+" cups of coffee")
+
 
