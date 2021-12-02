5d14d4c143f73f6f0c0d7a42ae18bed85fd723c3
Ihor <lytghzys@gmail.com>
2021-12-02 06:42:13 +0300

    Stage 3

--- dinnerparty/DinnerParty.py
+++ dinnerparty/DinnerParty.py
@@ -1,3 +1,5 @@
+import random
+
 NUMBER = int(input("Enter the number of friends joining (including you): "))
 
 NAMES = {}
@@ -5,11 +7,16 @@ if NUMBER == 0:
     print("No one is joining for the party")
 else:  
     for i in range(NUMBER):
-        NAMES[input("Enter the name of every friend (including you), each on a new line:")] = 0
+        NAMES[input("Enter the name of every friend (including you), each on a new line: ")] = 0
     TOTAL = int(input("Enter the total amount: "))
-    for i in NAMES:
-        NAMES[i] = round(TOTAL/NUMBER,2) 
-    print(NAMES)
+    ANSWER = input("Do you want to use the 'Who is lucky?' feature? Write y/n: ")
+    if ANSWER == 'y':
+        print(str(random.choice(list(NAMES))) + " is the lucky one!" )
+    else:
+        print("No one is going to be lucky")
+        for i in NAMES:
+            NAMES[i] = round(TOTAL/NUMBER,2) 
+        print(NAMES)
 
 
     
