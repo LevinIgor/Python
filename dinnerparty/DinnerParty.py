cf4932d1d922c6398188e6bec576810a4e04812d
Ihor <lytghzys@gmail.com>
2021-12-02 06:35:36 +0300

    Stage 2

--- dinnerparty/DinnerParty.py
+++ dinnerparty/DinnerParty.py
@@ -1,10 +1,15 @@
-number = int(input("Enter the number of friends joining (including you):"))
+NUMBER = int(input("Enter the number of friends joining (including you): "))
 
-NAMES = []
-if number == 0:
+NAMES = {}
+if NUMBER == 0:
     print("No one is joining for the party")
 else:  
-    for i in range(number):
-        NAMES.append(input("Enter the name of every friend (including you), each on a new line:"))
+    for i in range(NUMBER):
+        NAMES[input("Enter the name of every friend (including you), each on a new line:")] = 0
+    TOTAL = int(input("Enter the total amount: "))
+    for i in NAMES:
+        NAMES[i] = round(TOTAL/NUMBER,2) 
     print(NAMES)
+
+
     
