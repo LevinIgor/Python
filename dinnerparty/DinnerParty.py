6bfd2f8d3498987527a636ae7e7f476a33eddcef
Ihor <lytghzys@gmail.com>
2021-12-02 06:24:38 +0300

    State 1.1

--- dinnerparty/DinnerParty.py
+++ dinnerparty/DinnerParty.py
@@ -1,8 +1,10 @@
 number = int(input("Enter the number of friends joining (including you):"))
 
 NAMES = []
-
-for i in range(number):
-    NAMES.append(input("Enter the name of every friend (including you), each on a new line:"))
-print(NAMES)
+if number == 0:
+    print("No one is joining for the party")
+else:  
+    for i in range(number):
+        NAMES.append(input("Enter the name of every friend (including you), each on a new line:"))
+    print(NAMES)
     
