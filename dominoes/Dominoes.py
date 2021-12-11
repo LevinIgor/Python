7fe9ee6f2282c075ed24b6cb03cdaff24031d58e
Ihor <lytghzys@gmail.com>
2021-12-11 22:10:04 +0200

    stage 5

--- dominoes/Dominoes.py
+++ dominoes/Dominoes.py
@@ -1,5 +1,6 @@
 import random
 import sys
+import math
 DOMINOES = [[2,5],[1,2],[3,6],[0,0],[0,2],[5,6],[3,5],[2,4],[3,4],[1,5],[0,4],[2,6],[3,3],[1,1],[1,4],[1,3],[2,3],[4,5],[2,2],[0,3],[0,6],[5,5],[4,4],[4,6],[0,1],[0,5],[1,6],[6,6]]
 DOMINOES_copy = [[2,5],[1,2],[3,6],[0,0],[0,2],[5,6],[3,5],[2,4],[3,4],[1,5],[0,4],[2,6],[3,3],[1,1],[1,4],[1,3],[2,3],[4,5],[2,2],[0,3],[0,6],[5,5],[4,4],[4,6],[0,1],[0,5],[1,6],[6,6]]
 dominoesField = []
@@ -79,34 +80,34 @@ while True:
         print("Количество костей у компьютера: " + str(len(computerDominoes)))
         print("Количество костей в куче: "+str(len(DOMINOES)))
         for i in range(len(playerDominoes)):
-            print(str(i)+":"+str(playerDominoes[i]))
+            print(str(i+1)+":"+str(playerDominoes[i]))
         try:
             selectIndex = int(input("Введите номер кости: "))
-            if selectIndex > len(playerDominoes)-1:
+            if math.fabs(selectIndex)-1 > len(playerDominoes)-1:
                 print("Номер вышел за пределы")
             else:
                 if selectIndex > 0:
-                    if playerDominoes[selectIndex][0] == dominoesField[len(dominoesField)-1][1]:
-                        addToEnd(playerDominoes[selectIndex])
-                        playerDominoes.pop(selectIndex)
+                    if playerDominoes[selectIndex-1][0] == dominoesField[len(dominoesField)-1][1]:
+                        addToEnd(playerDominoes[selectIndex-1])
+                        playerDominoes.pop(selectIndex-1)
                         currentTurn = "computer"
                     else:
-                        print("Невозможно установить кость")
-                elif selectIndex <= 0:
-                    if playerDominoes[selectIndex*-1][1] == dominoesField[len(dominoesField)-1][0]:
-                        addToStart(playerDominoes[selectIndex*-1])
-                        playerDominoes.pop(selectIndex*-1)
-                        currentTurn = "computer"
-                    else:
-                        print("Невозможно установить кость")
-                elif selectIndex == 99:
+                        print("Невозможно установить! кость")
+                elif selectIndex == 0:
                     if len(DOMINOES)>0:
                         takeDominoes(playerDominoes)
                     else:
                         print("Куча пуста")
-                        if input("Для завершения нажмите 0") == 0:
+                        if input("Для завершения нажмите 0 - ") == 0:
                             sys.exit()
                     currentTurn = "computer"
+                elif selectIndex < 0:
+                    if playerDominoes[selectIndex*-1-2][1] == dominoesField[len(dominoesField)-1][0]:
+                        addToStart(playerDominoes[selectIndex*-1-1])
+                        playerDominoes.pop(selectIndex*-1-1)
+                        currentTurn = "computer"
+                    else:
+                        print("Невозможно установить кость!")
                 
         except ValueError:
             print("Неверно введеные данные")  
@@ -115,16 +116,8 @@ while True:
         computerMove()
         currentTurn = "player"
     if len(playerDominoes) == 0:
-        print("")
+        print("Вы выиграли")
+        sys.exit()
     if len(computerDominoes) == 0:
-        print("")
-
-
-
-
-    
-
-
-
-    
-
+        print("Компьютер выиграл")
+        sys.exit()
